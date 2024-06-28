cdace81838c5539f65532f1b04b4f2195cb69c9e
newlearnerds <118935542+newlearnerds@users.noreply.github.com>
2024-06-28 15:13:16 +0000

    test function

---
+++ .devcontainer/max_recur.py
@@ -0,0 +1,17 @@
+def max_recur(s):
+    n = len(s)
+    if n == 1:
+        return s[n-1]
+    
+    i = n-1
+    if s[i] > s[i-1]:
+        s = s[0:i-1]+s[i:]
+        print('1st check true:',s)
+        return max_recur(s)
+    else:
+        s = s[0:i]
+        print('2nd check true:',s)
+        return max_recur(s)
+    
+a = max_recur([2,5,1,6,3])
+print(a)   
