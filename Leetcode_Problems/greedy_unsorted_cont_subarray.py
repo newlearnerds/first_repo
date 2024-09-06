b9dc31c27e2ffd54534734a1a50260ac198e1d30
newlearnerds <118935542+newlearnerds@users.noreply.github.com>
2024-09-06 14:49:24 +0000

    greedy algo leetcode 581

--- Leetcode_Problems/greedy_unsorted_cont_subarray.py
+++ Leetcode_Problems/greedy_unsorted_cont_subarray.py
@@ -1,3 +1,11 @@
+"""
+Given an integer array nums, you need to find one continuous subarray such that if you only 
+sort this subarray in non-decreasing order, then the whole array will be sorted in non-decreasing order.
+
+Return the shortest such subarray and output its length.
+
+"""
+
 def findUnsortedSubarray(nums):
         
     minimum, maximum = float('inf'), float('-inf')
